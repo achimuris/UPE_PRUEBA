2b5c75b809fbd24bce8fbe50ae42fd899c2219a9
achimuris <chimuris@gmail.com>
2018-04-07 19:29:51 -0300

    Versión Inicial
    Versión Inicial

---
+++ miPrimeraAplicacionDeConsola/miPrimeraAplicacionDeConsola/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace miPrimeraAplicacionDeConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double nota1=0, nota2=0, nota3=0, nota4=0;
+            double promedio = 0;
+            
+            /*
+                int soyUnINT;
+                DateTime soyUnDATE;
+                bool soyUnBOOL;
+                char soyUnCHAR;
+                string soyUnSTRING;
+            */
+
+
+           
+
+            Console.WriteLine("Escriba la primera nota: ");
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escriba la segunda nota: ");
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escriba la tercer nota: ");
+            nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escriba la cuarta nota: ");
+            nota4 = double.Parse(Console.ReadLine());
+
+
+            promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            Console.WriteLine("El promedio es: " + promedio);
+            Console.WriteLine();
+            Console.WriteLine("Toque una tecla para salir");
+            Console.ReadLine();
+        }
+    }
+}
